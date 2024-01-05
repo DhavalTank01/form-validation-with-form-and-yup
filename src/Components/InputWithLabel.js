ce9e50a38312b91cde46275f3e4f75e8705d5a13
Tank Dhaval <tankdhaval2001@gmail.com>
2024-01-05 17:28:49 +0530

    update form

--- src/Components/InputWithLabel.js
+++ src/Components/InputWithLabel.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 const InputWithLabel = ({ label, id, type, value, placeholder, onChange, onBlur, isRemoveWhiteSpace = false, isUpperCase = false, allowOnlyString = false, error, ...rest }) => {
+    const [isShowPassword, setIsShowPassword] = useState(false);
     const handleOnChange = (e) => {
         let inputValue = e.target.value;
         let { name, id } = e.target;
@@ -20,22 +23,39 @@ const InputWithLabel = ({ label, id, type, value, placeholder, onChange, onBlur,
         onChange({ ...e, target: { id, name, value: inputValue } });
     };
 
+    const handleShowHidePassword = () => {
+        setIsShowPassword(prev => !prev);
+    }
+
+
+
     return (
         <div className="mb-3">
             <label htmlFor={id} className="form-label">
                 {label}
             </label>
-            <input
-                type={type}
-                id={id}
-                name={id}
-                onChange={handleOnChange}
-                onBlur={onBlur}
-                value={value}
-                placeholder={placeholder}
-                className="form-control"
-                {...rest}
-            />
+            <div className="d-flex gap-2 align-items-center  justify-content-center">
+                <input
+                    type={type === "password" ? isShowPassword ? "text" : "password" : type}
+                    id={id}
+                    name={id}
+                    onChange={handleOnChange}
+                    onBlur={onBlur}
+                    value={value}
+                    placeholder={placeholder}
+                    className="form-control"
+                    {...rest}
+                />
+                {type === "password" ? (
+                    <div className={`border border-1 p-1 rounded-1 ${isShowPassword ? "border-danger-subtle" : "border-success-subtle"}`} style={{ cursor: "pointer" }}>
+                        <FontAwesomeIcon
+                            color={isShowPassword ? "lightcoral" : "green"}
+                            onClick={handleShowHidePassword}
+                            icon={isShowPassword ? faEye : faEyeSlash}
+                        />
+                    </div>
+                ) : null}
+            </div>
             {error && (
                 <div id="emailHelp" className="form-text text-danger">
                     {error}
--- src/UseIdExample.js
+++ src/UseIdExample.js
@@ -12,12 +12,17 @@ const UseIdExample = () => {
             username: '',
             email: '',
             password: '',
+            confirm_password: '',
         },
         validationSchema: Yup.object({
             full_name: Yup.string().required('Full name is required'),
             username: Yup.string().required('Username is required'),
             email: Yup.string().email('Invalid email address').required('Email is required'),
             password: Yup.string().min(8, 'Password must be at least 8 characters').required('Password is required'),
+            confirm_password: Yup.string()
+                .oneOf([Yup.ref('password'), null], 'Passwords must match')
+                .min(8, 'Confirm password must be at least 8 characters')
+                .required('Confirm password is required'),
         }),
         onSubmit: (values) => {
             console.log(values);
@@ -27,54 +32,65 @@ const UseIdExample = () => {
 
     return (
         <div className="container mt-5">
-            <div className="border border-1 p-3 rounded-2">
-                <form onSubmit={formik.handleSubmit}>
-                    <InputWithLabel
-                        label="Full Name"
-                        id={`${id}-full_name`}
-                        name="full_name"
-                        type="full_name"
-                        placeholder="Please enter full name"
-                        {...formik.getFieldProps("full_name")}
-                        error={formik.touched.full_name && formik.errors.full_name}
-                        allowOnlyString={true}
-                    />
-                    <InputWithLabel
-                        label="Username"
-                        id={`${id}-username`}
-                        name="username"
-                        type="username"
-                        placeholder="Please enter username"
-                        {...formik.getFieldProps("username")}
-                        error={formik.touched.username && formik.errors.username}
-                        isUpperCase={true}
-                        isRemoveWhiteSpace={true}
-                    />
-                    <InputWithLabel
-                        label="Email"
-                        id={`${id}-email`}
-                        name="email"
-                        type="email"
-                        placeholder="Please enter email"
-                        {...formik.getFieldProps("email")}
-                        error={formik.touched.email && formik.errors.email}
-                    />
-                    <InputWithLabel
-                        label="Password"
-                        id={`${id}-password`}
-                        name="password"
-                        type="password"
-                        placeholder="Please enter password"
-                        {...formik.getFieldProps("password")}
-                        error={formik.touched.password && formik.errors.password}
-                    />
-                    <CustomButton
-                        text='Save'
-                        type='submit'
-                        disabled={Boolean(Object.keys(formik.errors)?.length)}
-                        isFullWidth={true}
-                    />
-                </form>
+            <div className='d-flex flex-column align-items-center justify-content-center'>
+                <div className="border border-1 p-3 rounded-2 w-50">
+                    <form onSubmit={formik.handleSubmit}>
+                        <InputWithLabel
+                            label="Full Name"
+                            id={`${id}-full_name`}
+                            name="full_name"
+                            type="full_name"
+                            placeholder="Please enter full name"
+                            {...formik.getFieldProps("full_name")}
+                            error={formik.touched.full_name && formik.errors.full_name}
+                            allowOnlyString={true}
+                        />
+                        <InputWithLabel
+                            label="Username"
+                            id={`${id}-username`}
+                            name="username"
+                            type="username"
+                            placeholder="Please enter username"
+                            {...formik.getFieldProps("username")}
+                            error={formik.touched.username && formik.errors.username}
+                            isUpperCase={true}
+                            isRemoveWhiteSpace={true}
+                        />
+                        <InputWithLabel
+                            label="Email"
+                            id={`${id}-email`}
+                            name="email"
+                            type="email"
+                            placeholder="Please enter email"
+                            {...formik.getFieldProps("email")}
+                            error={formik.touched.email && formik.errors.email}
+                        />
+                        <InputWithLabel
+                            label="Password"
+                            id={`${id}-password`}
+                            name="password"
+                            type="password"
+                            placeholder="Please enter password"
+                            {...formik.getFieldProps("password")}
+                            error={formik.touched.password && formik.errors.password}
+                        />
+                        <InputWithLabel
+                            label="Confirm Password"
+                            id={`${id}-confirm_password`}
+                            name="confirm_password"
+                            type="password"
+                            placeholder="Please enter confirm password"
+                            {...formik.getFieldProps("confirm_password")}
+                            error={formik.touched.confirm_password && formik.errors.confirm_password}
+                        />
+                        <CustomButton
+                            text='Save'
+                            type='submit'
+                            disabled={Boolean(Object.keys(formik.errors)?.length)}
+                            isFullWidth={true}
+                        />
+                    </form>
+                </div>
             </div>
         </div>
     )
